7acb6b90959081e563f8191a700ed791d5b94071
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2022-04-19 12:57:05 -0500

    added base for a lot of the front end axios call connecting the backend started the video player with iframe and made the video change depending on a variable..

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,36 @@
-import './App.css';
-import React, { useState } from 'react';
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
-  const comments;
+  const [Comments, setComments] = useState([]);
+  const [video, setVideo] = useState({});
+  video.id= 'ZJPju9tOJ_I'
   
+  const getComments = async () => {
+    try {
+      let response = await axios.get("http://localhost:3007/api/comments");
+      console.log(response.data);
 
-
+      setComments(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <button onClick={getComments}>Click for Comments</button>
+      <div className="videoplayer">Video</div>
+      <iframe
+        id="ytplayer"
+        type="text/html"
+        width="640"
+        height="360"
+        src={`https://www.youtube.com/embed/${video.id}`}
+      ></iframe>
+      <div>{video.description}</div>
     </div>
   );
 }
